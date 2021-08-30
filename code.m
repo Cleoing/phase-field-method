4286670054dc89c3eaaf6886ad1fd00b6711d652
Siliang Liu <79851602+Cleoing@users.noreply.github.com>
2021-08-30 16:52:08 +0200

    Update code.m

--- code.m
+++ code.m
@@ -1,3 +1,7 @@
+$ git config --global core.autocrlf true
+# Configure Git to ensure line endings in files you checkout are correct for Windows.
+# For compatibility, line endings are converted to Unix style when you commit files.
+
 clear;clc;clf;tic
 
 %-----------basic set up------------
